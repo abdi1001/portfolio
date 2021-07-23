4634506482d35406a1b5a64c29e775c7c603c81d
Abdi <abdifatah952@gmail.com>
2021-07-22 22:51:27 -0500

    initial deploy to github pages

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={'topbar ' + (menuOpen && 'active')}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+952 555 5555</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>Abdi@test.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
